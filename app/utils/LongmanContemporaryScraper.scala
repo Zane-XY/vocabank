f9f1e4c1218175aa94a10a40de7c5439cbafc3cf
Zane.XY <zane.xy@live.com>
2014-06-24 22:17:46 +0800

    + longman scraper + oxford scraper

---
+++ app/utils/LongmanContemporaryScraper.scala
@@ -0,0 +1,32 @@
+package utils
+
+import org.jsoup.Jsoup
+import org.jsoup.safety.Whitelist
+import scala.collection.JavaConversions._
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+/**
+ * Created by zane.wang on 6/23/2014.
+ */
+object LongmanContemporaryScraper {
+  val dict = "http://www.ldoceonline.com"
+  val search = "/search/?q="
+  val entry = ""
+
+  def scrapeEntry(link : String) =  {
+    Jsoup.clean(Jsoup.connect(link).get.select("div.Entry").toString, Whitelist.relaxed().addAttributes("div","class"))
+  }
+
+  def scrape(word : String) : String = {
+   val res = Jsoup.connect(dict + search + word).ignoreHttpErrors(true).execute
+   res.statusCode match {
+      case 200 => val doc = res.parse().select("a:has(span:matchesOwn(^" + word + "$))")
+                  val urls = doc.iterator.toList.map(dict + _.attr("href"))
+                  //Future.traverse(urls)(scrapeEntry).value.get.get.mkString("\n")
+                  urls.par.map(scrapeEntry).mkString("\n")
+      case _ => ""
+    }
+  }
+
+}
---
+++ app/utils/OxfordLearnerScraper.scala
@@ -0,0 +1,22 @@
+package utils
+
+import org.jsoup.Jsoup
+
+/**
+ * Created by zane.wang on 6/23/2014.
+ */
+object OxfordLearnerScraper {
+  val dict = "http://www.oxfordlearnersdictionaries.com/us/definition/american_english/"
+
+  def scrape(word : String) : String = {
+   val res = Jsoup.connect(dict + word).ignoreHttpErrors(true).execute
+   res.statusCode match {
+      case 200 => val doc = res.parse().select("#ox-container")
+                  val entryContent = doc.select("div#entryContent")
+                  val relatedEntries = doc.select("relatedentries")
+                  entryContent.toString + relatedEntries.toString
+      case _ => ""
+    }
+  }
+
+}
