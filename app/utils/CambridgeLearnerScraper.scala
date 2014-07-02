2a80c16a68ec8065077fcf2e7d739872ccc98eec
Zane.XY <zane.xy@live.com>
2014-07-02 22:19:27 +0800

    update cambridge scraper to handle special chars

--- app/utils/CambridgeLearnerScraper.scala
+++ app/utils/CambridgeLearnerScraper.scala
@@ -1,6 +1,7 @@
 package utils
 
 import org.jsoup.Jsoup
+import play.api.Logger
 
 /**
  * Created by zane.wang on 6/23/2014.
@@ -9,13 +10,14 @@ object CambridgeLearnerScraper {
   val dict = "http://dictionary.cambridge.org/dictionary/learner-english/"
 
   def scrape(word : String) : String = {
-   val res = Jsoup.connect(dict + word).timeout(60*1000).ignoreHttpErrors(true).execute
+   val t = word.replaceAll("[\\s|']+", "-").toLowerCase
+   val res = Jsoup.connect(dict + t).timeout(60*1000).ignoreHttpErrors(true).execute
    res.statusCode match {
       case 200 => val doc = res.parse().select("div.di").addClass("entrybox learner-english")
                   doc.select("div.share-this-entry").remove
                   doc.select("div.di-title").remove
                   doc.toString
-        case _ => ""
+      case _ => ""
     }
   }
 
