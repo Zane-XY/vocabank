475f0f438ab4e2eb4e119b9b0567359501210c08
Zane.XY <zane.xy@live.com>
2014-07-04 17:13:46 +0800

    + signInAuth controller

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -22,33 +22,64 @@ object UserController extends Controller {
     })
   )
 
+  val signInF = Form(
+    tuple(
+      "email" -> email,
+      "password" -> nonEmptyText
+    )
+  )
+
   val userF = Form[User](
     mapping(
       "name" -> nonEmptyText,
       "password" -> nonEmptyText,
       "email" -> email)(
         (name, password, email) => User(None, name, email, password, new DateTime))
-      ((u: User) => Some(u.name, "", u.email)))
+        ((u: User) => Some(u.name, "", u.email))
+  )
 
   def signUp = Action { implicit req =>
     Ok(views.html.signUp(userF))
   }
 
+  def signIn = Action { implicit req =>
+    Ok(views.html.signIn(signInF))
+  }
 
-  def save = CSRFCheck { Action { implicit req =>
-    val uf = userF.bindFromRequest
-    uf.fold(
-    _ => (Ok(views.html.signUp(uf))), { case user: User =>
-      captchaF.bindFromRequest.fold(
-        _ => (Ok(views.html.signUp(userF.fill(user)))),
-        _ => {
-          User.save(user)
-          Redirect(routes.EntryController.entries).flashing("success" -> "Entry saved")
-        }
-      )
+  def signInAuth = CSRFCheck {
+    Action { implicit req =>
+      val signInForm = signInF.bindFromRequest
+      signInForm.fold(
+        _ => (Ok(views.html.signIn(signInForm))),
+        { case t @ (e, p) =>
+            captchaF.bindFromRequest.fold(
+              _ => Ok(views.html.signIn(signInForm)),
+              _ => {
+                val (signedIn, msg) = User.auth(e, p)
+                if (signedIn)
+                  Redirect(routes.EntryController.entries).flashing("msg" -> msg)
+                else
+                  Redirect(routes.UserController.signIn).flashing(Flash(signInForm.data) + ("msg" -> msg))
+              }
+            )
+        })
     }
-    )
-   }
+  }
+
+  def save = CSRFCheck {
+    Action { implicit req =>
+      val uf = userF.bindFromRequest
+      uf.fold(
+        _ => (Ok(views.html.signUp(uf))),
+        { case user: User =>
+              captchaF.bindFromRequest.fold(
+                _ => Ok(views.html.signUp(userF)),
+                _ => {
+                  User.save(user)
+                  Redirect(routes.EntryController.entries).flashing("success" -> "Entry saved")
+                })
+        })
+    }
   }
 
 }
