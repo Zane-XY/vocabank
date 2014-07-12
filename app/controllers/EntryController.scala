f23510f241ec5c27662d7bcf48f2fcbfa37898d7
Zane.XY <zane.xy@live.com>
2014-07-12 20:04:06 +0800

    ~ DB ops only take JSON request

--- app/controllers/EntryController.scala
+++ app/controllers/EntryController.scala
@@ -1,65 +1,43 @@
 package controllers
 
+import controllers.Common._
+import models._
 import play.api._
 import play.api.data.Form
 import play.api.data.Forms._
-import play.api.i18n.Messages
-import play.api.i18n.Messages.Message
-import play.api.libs.Comet
-import play.api.libs.concurrent.Promise
-import play.api.libs.iteratee.Enumerator
-import play.api.libs.json.{JsError, Json}
+import play.api.libs.json.Json
 import play.api.mvc._
-import models._
-
-import org.joda.time.DateTime
-
-import play.filters.csrf._
 import play.filters.csrf.CSRF.Token._
-import utils.{OxfordLearnerScraper, LongmanContemporaryScraper, CambridgeLearnerScraper}
-import scala.concurrent.{Await, Future}
-import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.Try
-
-//import play.api.libs.json._
-//import play.api.libs.json.Reads._
-//import play.api.libs.functional.syntax._
+import play.filters.csrf._
 
 object EntryController extends Controller {
 
-  def formToEntry(id: Option[Long], title: String, source: Option[List[String]], content: String, tags: Option[List[String]], rating: Int): Entry = {
-    val now = new DateTime
-    Entry(None, title, source, content, now, now, tags, rating, 0L)
+  val entryF = Form(
+    mapping(
+      "id" -> optional(longNumber),
+      "title" -> nonEmptyText,
+      "source" -> optional(list(text)),
+      "content" -> text,
+      "tags" -> optional(list(text)),
+      "rating" -> number(min = 1, max = 5)
+    )(Entry.assemble)(Entry.disassemble))
+
+  def entries = Action { implicit req =>
+    userIdFromSession.fold(
+      BadRequest(views.html.errors("error.user.not.signedIn")))(userId =>
+      Ok(views.html.entries(Entry.listAll(userId)))
+    )
   }
 
-  def entryToForm(e: Entry) = Some( e.id, e.title, e.source, e.content, e.tags, e.rating)
-
-  val entryF = Form(mapping(
-    "id" -> optional(longNumber),
-    "title" -> nonEmptyText,
-    "source" -> optional(list(text)),
-    "content" -> text,
-    "tags" -> optional(list(text)),
-    "rating" -> number(min = 1, max = 5))(formToEntry)(entryToForm))
-
-  //entryF("rating").value
-
-  def entries = Action {
-    implicit request =>
-    Ok(views.html.entries(Entry.listAll))
-  }
-
-  def add = Action { implicit request =>
-    Ok(views.html.entry(entryF))
-  }
-
-  def delete(id:Long) = Action { implicit request =>
-    Ok(views.html.entry(entryF))
-  }
-
-  def edit(id:Long) = Action { implicit request =>
-    Ok(views.html.entry(entryF))
+  def delete = CSRFCheck {
+    Action(parse.json) { implicit req =>
+      import controllers.JsonValidators.entryIdReads
+      userIdFromSession.fold(NotSignedIn)(userId =>
+        req.body.validate[Long].map {
+          case id => Ok(Json.obj("status" -> "OK", "rows deleted" -> Entry.delete(id, userId)))
+        }.recoverTotal(BadRequestJSON)
+      )
+    }
   }
 
   /**
@@ -68,12 +46,11 @@ object EntryController extends Controller {
    */
   def setRating = CSRFCheck {
     Action(parse.json) { implicit req =>
-      import JsonValidators.ratingReads
-      req.body.validate[(Long, Int)].map {
-        case (id, value) => Ok(Json.obj( "status" -> "OK", "updated" -> Entry.setRating(id, value)))
-      }.recoverTotal{
-        e => BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(e)))
-      }
+      import controllers.JsonValidators.ratingReads
+      userIdFromSession.fold(NotSignedIn)(userId =>
+        req.body.validate[(Long, Int)].map {
+          case (id, value) => Ok(Json.obj( "status" -> "OK", "updated" -> Entry.setRating(id, value, userId)))
+        }.recoverTotal(BadRequestJSON))
    }
   }
 
@@ -83,11 +60,7 @@ object EntryController extends Controller {
    */
   def save = CSRFCheck {
     Action (parse.json) { implicit req =>
-      import JsonValidators.entryReads
-      req.session.get("userId").flatMap(s => Try{s.toLong}.toOption).fold(
-        BadRequest(Json.obj("status" -> "KO" , "error" -> Messages("error.user.not.signedIn")))
-      )(
-        userId =>
+      userIdFromSession.fold(NotSignedIn)(userId =>
           entryF.bindFromRequest.fold(
             err => BadRequest(err.errorsAsJson),
             entry => {
@@ -119,17 +92,25 @@ object EntryController extends Controller {
 }
 
 object JsonValidators {
-  import play.api.libs.json._
-  import play.api.libs.json.Reads._
   import play.api.libs.functional.syntax._
+  import play.api.libs.json.Reads._
+  import play.api.libs.json._
 
   implicit val ratingReads : Reads[(Long, Int)] = (
     (__ \ "id").read[Long] and
     (__ \ "value").read[Int](min(1) keepAnd max(5))
   ) tupled
 
-  implicit val entryReads : Reads[(String, String)] = (
-    (__ \ "title").read[String] and
-      (__ \ "content").read[String]
-    ) tupled
+  implicit  val entryReads:Reads[Entry] = (
+      (__ \ "id").read[Option[Long]] and
+      (__ \ "title").read[String] and
+      (__ \ "source").read[Option[List[String]]] and
+      (__ \ "content").read[String] and
+      (__ \ "tags").read[Option[List[String]]] and
+      (__ \ "rating").read[Int]
+    )(Entry.assemble _)
+
+
+  implicit val entryIdReads : Reads[Long] = (__ \ "id").read[Long]
+
 }
