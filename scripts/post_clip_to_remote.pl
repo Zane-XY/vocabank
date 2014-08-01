f58ab47d6a0767c57468423fe83cf1f1d8caa2f6
Zane.XY <zane.xy@live.com>
2014-08-01 15:19:50 +0800

    ~ post script

--- scripts/post_clip_to_remote.pl
+++ scripts/post_clip_to_remote.pl
@@ -4,19 +4,18 @@ use LWP::UserAgent;
 use JSON;
 use Config::Tiny;
 
-my @lines = <STDIN>;
+#my @lines = <STDIN>;
+my @lines = ("headword", "content");
 
 my $config = Config::Tiny->read('config.ini');
 my $email = $$config{_}{email};
 my $pass =  $$config{_}{pass};
+my $uri = $$config{_}{uri};
 
 my ($headword, @context) = @lines;
 
 sub send_to_remote {
-  my $uri = 'http://localhost:9000/entry/remoteSave';
-
   my $json = {headword => $headword, context => join('', @context), rating => 1};
-
   my $req = HTTP::Request->new('POST', $uri);
   $req->header('Content-Type' => 'application/json');
   $req->context(JSON->new->encode($json));
@@ -28,5 +27,7 @@ sub send_to_remote {
 
 if(@context){
     send_to_remote();
+} else {
+    print "skipped due to empty content", "\n";
 }
 
