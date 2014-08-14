3a1767ea7bec90438011db15cd0d9be3829e34f9
Zane.XY <zane.xy@live.com>
2014-08-14 14:13:52 +0800

    + sound scraper

---
+++ app/utils/SoundScraper.scala
@@ -0,0 +1,18 @@
+package utils
+
+import org.jsoup.Jsoup
+
+/**
+ * Created by zane.wang on 8/14/2014.
+ */
+object SoundScraper {
+  val src = "http://www.oxforddictionaries.com/us/definition/american_english/"
+
+  def scrape(word : String) : (String) = {
+    val res = Jsoup.connect(src + word).timeout(60 * 1000).ignoreHttpErrors(true).execute
+    res.statusCode match {
+      case 200 => res.parse().select("div.sound").attr("data-src-mp3")
+      case _ => ""
+    }
+  }
+}
