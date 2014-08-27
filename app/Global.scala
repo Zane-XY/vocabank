f9a744366a3df7e37a516bd4811c4a8f30b77fe2
Zane.XY <zane.xy@live.com>
2014-08-27 15:04:51 +0800

    + tags to global cache

--- app/Global.scala
+++ app/Global.scala
@@ -1,7 +1,14 @@
+import models.Entry
 import play.api._
+import play.api.cache.Cache
 import play.api.mvc._
 import play.filters.csrf._
+import play.api.Play.current
 
 object Global extends WithFilters(CSRFFilter()) with GlobalSettings {
   // ... onStart, onStop etc
+  override def onStart(app: Application) {
+    Logger.info("loading tags into cache")
+    Cache.set("tags", Entry.loadTags)
+  }
 }
