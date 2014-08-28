50b53e9704d99ce09616d7bc911fb9d8c425e193
Zane.XY <zane.xy@live.com>
2014-08-28 16:50:51 +0800

    SoundScraper tries different sources if failed

--- app/utils/SoundScraper.scala
+++ app/utils/SoundScraper.scala
@@ -1,18 +1,35 @@
 package utils
 
 import org.jsoup.Jsoup
+import scalaz._
+import std.option._
+import syntax.plus._
 
 /**
  * Created by zane.wang on 8/14/2014.
  */
-object SoundScraper {
-  val src = "http://www.oxforddictionaries.com/us/definition/american_english/"
 
-  def scrape(word : String) : Option[String] = {
-    val res = Jsoup.connect(src + word).timeout(60 * 1000).ignoreHttpErrors(true).execute
+object SoundScraper {
+  val oxford = "http://www.oxforddictionaries.com/us/definition/american_english/"
+  val cambridge = "http://dictionary.cambridge.org/us/dictionary/british/"
+  
+  def scrape(word : String) : Option[String] =  scrapeOxford(word) <+> scrapeCambridge(word)
+
+  def scrapeOxford(word : String) : Option[String] = {
+    val res = Jsoup.connect(oxford + word).timeout(60 * 1000).ignoreHttpErrors(true).execute
     res.statusCode match {
       case 200 => Some(res.parse().select("div.sound").attr("data-src-mp3"))
       case _ => None
     }
   }
+
+  def scrapeCambridge(word : String) : Option[String] = {
+    val res = Jsoup.connect(cambridge + word).timeout(60 * 1000).ignoreHttpErrors(true).execute
+    res.statusCode match {
+      case 200 => Some(res.parse().select("a.pron-us").attr("data-src-mp3"))
+      case _ => None
+    }
+  }
+
 }
+
