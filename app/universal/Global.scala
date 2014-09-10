1f300d8fbe48f24e14532d138ca926945f569470
Zane.XY <zane.xy@live.com>
2014-09-10 16:50:33 +0800

    move Global.scala to package

--- app/universal/Global.scala
+++ app/universal/Global.scala
@@ -1,16 +1,15 @@
+package universal
+
 import models.Entry
 import play.api._
-import play.api.cache.Cache
 import play.api.mvc._
 import play.filters.csrf._
-import play.api.Play.current
-
-import scala.concurrent.duration._
 
 object Global extends WithFilters(CSRFFilter()) with GlobalSettings {
+
+  lazy val tagsCache = Entry.loadTags
   // ... onStart, onStop etc
   override def onStart(app: Application) {
-    Logger.debug("loading tags into cache" + Entry.loadTags)
-    Cache.set("tags", Entry.loadTags, expiration = 365.days)
   }
+
 }
